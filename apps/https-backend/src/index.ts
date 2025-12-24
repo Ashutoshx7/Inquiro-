f957dd7662eedeb5ebdcbf202d271af4a71e6fe4
Karn Ashutosh  Singh <karns7844@gmail.com>
2025-12-24 07:41:40 +0530

    signup endpoint

--- apps/https-backend/src/index.ts
+++ apps/https-backend/src/index.ts
@@ -1,26 +1,9 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config"
 
 const app = express();
 
-app.post("/signup", (req, res) => {
-
-  
-});
-
-app.post("/signin", (req, res) => {
-
-    const userId=1;
+app.post("/", (req, res) => {
     
-    
-    const token=jwt.sign({
-        userId
-    },JWT_SECRET);
-
-    res.json({
-        token
-    })
 
 });
 
